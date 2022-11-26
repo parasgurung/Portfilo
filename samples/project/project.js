97dfe9ec867bc48915eefaedb0ab7d035b6a2b2e
paras gurungQ6 <gowrongparas321@gmail.com>
2022-11-26 13:11:33 +0545

    added files and folder of website

---
+++ samples/project/project.js
@@ -0,0 +1,11 @@
+console.log('paras gurung')
+    //rules for variable
+    //assume var  or let (best)
+    //cant not start with number, space or hyphen(_) e.g 1paras _james
+    //camel rule like var fistName is modern best practice e.g let firstName = paras
+let firstName = 'James';
+var lastName = 'Chetri'
+console.log(firstName + " " + lastName)
+let interest = 14; //use const insted let 
+interest = 20;
+console.log(interest)
